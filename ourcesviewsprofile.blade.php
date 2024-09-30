f58bfd185be82ccbba3e026c18dd4bb9f305fd3e
Theresia <theresiaoktavia2004@gmail.com>
2024-09-30 16:36:14 +0700

    Tugas 3

---
+++ ourcesviewsprofile.blade.php
@@ -0,0 +1,41 @@
+[33mcommit 1e1cbeb9713bafe15f03c34a0b6016fc0892dc1e[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mdev[m[33m, [m[1;31morigin/form-controller[m[33m, [m[1;32mform-controller[m[33m)[m
+Author: Theresia <theresiaoktavia2004@gmail.com>
+Date:   Mon Sep 30 14:36:04 2024 +0700
+
+    Tugas 3
+
+[33mcommit a3a12904653c1edf76379654a4363410b079cf40[m[33m ([m[1;31morigin/dev[m[33m)[m
+Merge: 3e9f2ca bf5b06a
+Author: Theresia Oktavia <112594407+TheresiaTriOktavia@users.noreply.github.com>
+Date:   Mon Sep 30 13:17:17 2024 +0700
+
+    Merge pull request #2 from TheresiaTriOktavia/routes-views
+    
+    Tugas 2
+
+[33mcommit bf5b06a271f99ffc34b843a7913ee6073df3eac1[m[33m ([m[1;31morigin/routes-views[m[33m)[m
+Author: Theresia <theresiaoktavia2004@gmail.com>
+Date:   Mon Sep 30 12:49:46 2024 +0700
+
+    Tugas 2
+
+[33mcommit 3e9f2ca168f6578b05cecf9f0acbd0ac78deb857[m
+Merge: 017cf9c 2980da1
+Author: Theresia Oktavia <112594407+TheresiaTriOktavia@users.noreply.github.com>
+Date:   Mon Sep 30 12:12:08 2024 +0700
+
+    Merge pull request #1 from TheresiaTriOktavia/landing-page
+    
+    Tugas 1
+
+[33mcommit 2980da1489283106b253ef2d7924082f08911d40[m[33m ([m[1;31morigin/landing-page[m[33m, [m[1;32mlanding-page[m[33m)[m
+Author: Theresia <theresiaoktavia2004@gmail.com>
+Date:   Mon Sep 30 12:10:39 2024 +0700
+
+    Tugas 1
+
+[33mcommit 017cf9ca3dc0d27fcf878b62bce81093f2a64b35[m[33m ([m[1;31morigin/master[m[33m, [m[1;32mmaster[m[33m)[m
+Author: Theresia <theresiaoktavia2004@gmail.com>
+Date:   Mon Sep 30 11:43:25 2024 +0700
+
+    Initial Commit
